d2e0bf563eda25a13eb477206006531d152d6de9
Nuradil <113079748+nuradil7704@users.noreply.github.com>
2023-01-05 11:40:06 +0600

    Add files via upload

---
+++ DZ-5_Nuradil_18-01/my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import UserListPage from "./pages/userListPage/UserListPage";
+import GetUser from "./component/getUser/GetUser";
+
+function App() {
+  return (
+    <BrowserRouter>
+        <GetUser/>
+        <Routes>
+          <Route index element={<UserListPage/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ DZ-5_Nuradil_18-01/my-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {rootReducer} from "./redux/reducers";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import thunk from "redux-thunk";
+import {applyMiddleware} from "redux";
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
